518e5ff87dba8bbf1bf1258c779b18c3d81fe2eb
sleepy <aliffarhan003@gmail.com>
2023-12-02 10:45:34 +0700

    update node&model Keranjang

---
+++ src/node/NodeClass/NodeKeranjang.java
@@ -0,0 +1,36 @@
+package node.NodeClass;
+
+import model.Fake;
+
+import java.util.ArrayList;
+
+public class NodeKeranjang {
+    private int Id;
+    private NodeUser user;
+    ArrayList<NodeBarang> listBarang;
+    int totalHarga;
+    public NodeKeranjang(NodeUser u) {
+        user = u;
+        Id = u.getId_user();
+        listBarang = new ArrayList<>();
+    }
+    public void addBarang(NodeBarang p) {
+        NodeBarang temp = new NodeBarang(p);
+        listBarang.add(temp);
+    }
+
+    public void deleteBarang(int id) {;
+        listBarang.remove(id);
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void viewbarang() {
+        for (NodeBarang g: listBarang) {
+            System.out.println(g.getId_barang());
+            System.out.println(g.getNamaBarang());
+        }
+    }
+}
