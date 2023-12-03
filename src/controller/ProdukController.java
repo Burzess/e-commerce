8a0307fbc7bd4f88cb21b9a74a3c15c790966c92
aui_tuthousend <mraihan286@gmail.com>
2023-12-03 16:56:46 +0700

    Add produk Model controller view

--- src/controller/ProdukController.java
+++ src/controller/ProdukController.java
@@ -4,6 +4,17 @@ import model.ModelProduk;
 import node.NodeClass.NodeProduk;
 
 public class ProdukController {
+    private int id = 0;
+    public void addProduk(String nB, int hg, String ktg, int stok){
+        if(ModelProduk.cekBarang(nB)){
+            NodeProduk produk = new NodeProduk(id, nB, hg, ktg, stok);
+            ModelProduk.addProdukModel(produk);
+            id++;
+            System.out.println("Produk Berhasil ditambah!!");
+        } else {
+            System.out.println("Nama barang sudah ada, gunakan nama lain :D");
+        }
+    }
     public void updateProduk(int id, String update){
         NodeProduk nodeProduk = ModelProduk.searchProduk(id);
         String[] stuff = update.split("/");
