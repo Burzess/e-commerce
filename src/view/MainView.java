196c7319a3c3cc93f0c7b855af7e9fb6274b4618
aui_tuthousend <mraihan286@gmail.com>
2024-01-02 18:47:25 +0700

    if add keranjang jumlah <= 0, return false

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -42,75 +42,7 @@ public class MainView {
 
     }
 
-    public static void viewKeranjang(NodeUser user){
-        boolean cek = keranjangController.vieww(user);
-        if (cek){
-            System.out.println("\nSaldo anda: "+ user.getSaldo());
-            System.out.println("Gunakan koma bila perlu (2,8,7): ");
-            System.out.println("Pilih id barang yang akan di checkout: ");
-            System.out.print("atau input n untuk kembali: ");
-            String barang = input.nextLine();
-            if (barang.equals("n")){
-                return;
-            }
-            transaksiController.addTransaksi(user, barang);
-            System.out.print("Ketik n untuk kembali ke beranda: ");
-            String bak = input.nextLine();
-        }
-    }
-
-    public static void sellBarang(NodeUser user){
-        produkView.addProdukView(user);
-    }
-
-    public static void getBarangDagangan(NodeUser user){
-        List<NodeProduk> barang = produkController.getDaganganUser(user);
-        if (barang!=null){
-            for (NodeProduk produk: barang){
-                System.out.println(produk.viewDataProduk());
-            }
-            System.out.print("""
-                               opsi: \s
-                               1. Edit Barang
-                               2. Kembali ke beranda
-                               Masukkan pilihan: \s""");
-            int op = input.nextInt();
-            input.nextLine();
-            if (op==1){
-                produkView.updateProduk();
-            }
-        }
-    }
-
-    public static void viewAllBarang(NodeUser user){
-        int c = 0;
-        for (NodeProduk produk: ModelProduk.produkList){
-            if (produk.getUser().getNama().equals(user.getNama())){
-                continue;
-            }
-//            System.out.println(produk.viewDataProduk());
-            produk.outData2();
-            c++;
-        }
-        if (c==0){
-            System.out.println("Barang tidak ada");
-            return;
-        }
-        System.out.println("\nMasukkan kode barang dan jumlah bila ingin menambah ke keranjang");
-        System.out.println("contoh: 3-1, 1-10");
-        System.out.print("atau input n untuk kembali: ");
-        String op = input.nextLine();
-        if (!op.equals("n")){
-            boolean stat = keranjangController.addProduk(user.getId_user(), op);
-            if (!stat){
-                System.out.println("Id barang not found/stok habis🙏");
-                return;
-            }
-            System.out.println("Berhasil menambah wishlist!");
-        }
-
-    }
-
+// ***************   case 1 **********************************
     public static void searchProduk(NodeUser user){
         int c = 0;
         System.out.print("""
@@ -144,18 +76,93 @@ public class MainView {
             System.out.println("Produk yang anda cari tidak ada");
         }
     }
+// ***************   case 2 **********************************
+    public static void viewAllBarang(NodeUser user){
+        int c = 0;
+        for (NodeProduk produk: ModelProduk.produkList){
+            if (produk.getUser().getNama().equals(user.getNama())){
+                continue;
+            }
+//            System.out.println(produk.viewDataProduk());
+            produk.outData2();
+            c++;
+        }
+        if (c==0){
+            System.out.println("Barang tidak ada");
+            return;
+        }
+        System.out.println("\nMasukkan kode barang dan jumlah bila ingin menambah ke keranjang");
+        System.out.println("contoh: 3-1, 1-10");
+        System.out.print("atau input n untuk kembali: ");
+        String op = input.nextLine();
+        if (!op.equals("n")){
+            boolean stat = keranjangController.addProduk(user.getId_user(), op);
+            if (!stat){
+                System.out.println("Id barang not found/stok habis/input jumlah anda salah🙏");
+                return;
+            }
+            System.out.println("Berhasil menambah wishlist!");
+        }
 
+    }
+// ***************   case 3 **********************************
+    public static void viewKeranjang(NodeUser user){
+        boolean cek = keranjangController.vieww(user);
+        if (cek){
+            System.out.println("\nSaldo anda: "+ user.getSaldo());
+            System.out.println("Gunakan koma bila perlu (2,8,7): ");
+            System.out.println("Pilih id barang yang akan di checkout: ");
+            System.out.print("atau input n untuk kembali: ");
+            String barang = input.nextLine();
+            if (barang.equals("n")){
+                return;
+            }
+            transaksiController.addTransaksi(user, barang);
+            System.out.print("Ketik n untuk kembali ke beranda: ");
+            String bak = input.nextLine();
+        }
+    }
+// ***************   case 4 **********************************
+    public static void viewAllUserTransaksi(NodeUser user){
+        ArrayList<NodeTransaksi> userTransaksi = transaksiController.viewUserTransaksi(user);
+        int c = 0;
+        for (NodeTransaksi transaksi: userTransaksi){
+            transaksi.view();
+            c++;
+        }
+        if (c==0){
+            System.out.println("Belum ada riwayat transaksi");
+        }
+        System.out.print("\nTekan n untuk kembali: ");
+        String back = input.nextLine();
+    }
+// ***************   case 5 **********************************
+    public static void sellBarang(NodeUser user){
+        produkView.addProdukView(user);
+    }
+// ***************   case 6 **********************************
+    public static void getBarangDagangan(NodeUser user){
+        List<NodeProduk> barang = produkController.getDaganganUser(user);
+        if (barang!=null){
+            for (NodeProduk produk: barang){
+                System.out.println(produk.viewDataProduk());
+            }
+            System.out.print("""
+                               opsi: \s
+                               1. Edit Barang
+                               2. Kembali ke beranda
+                               Masukkan pilihan: \s""");
+            int op = input.nextInt();
+            input.nextLine();
+            if (op==1){
+                produkView.updateProduk();
+            }
+        }
+    }
+// ***************   case 7 **********************************
     public static void addUser(){
         userView2.addUser();
     }
 
-    public static void viewAllUserTransaksi(NodeUser user){
-        ArrayList<NodeTransaksi> userTransaksi = transaksiController.viewUserTransaksi(user);
-        for (NodeTransaksi transaksi: userTransaksi){
-            transaksi.view();
-        }
-        System.out.print("Tekan n untuk kembali: ");
-        String back = input.nextLine();
-    }
 }
 
